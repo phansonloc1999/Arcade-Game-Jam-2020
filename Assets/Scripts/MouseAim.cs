e29b5496d0bdc9dbe3fc5fe70cb4c2fe2724c1a6
phansonloc1999 <phansonloc1999@gmail.com>
2020-10-12 16:31:47 +0700

    Add MouseAim

---
+++ Assets/Scripts/MouseAim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+    }
+}
