38424c40a584c2ef92d1f320157ba5d06ccb84e6
phansonloc1999 <phansonloc1999@gmail.com>
2020-10-30 14:05:56 +0700

    Update bomb left UI

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Bomb : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _animalsToBeBombed;
 
+    [SerializeField] private Text _bombText;
+
+    [SerializeField] private int _maxBomb;
+
+    [SerializeField] private int _bombLeft;
+
+    [SerializeField] private GameObject[] _bombImages;
+
     // Start is called before the first frame update
     void Start()
     {
         _animalsToBeBombed = new List<GameObject>();
+
+        _bombLeft = _maxBomb;
     }
 
     // Update is called once per frame
@@ -26,6 +37,20 @@ public class Bomb : MonoBehaviour
         foreach (var animal in _animalsToBeBombed)
         {
             Destroy(animal);
+        }
+
+        _bombLeft = Mathf.Max(_bombLeft - 1, 0);
+
+        _bombText.text = "Bomb " + _bombLeft;
+
+        for (int i = 0; i < _bombLeft; i++)
+        {
+            _bombImages[i].SetActive(true);
+        }
+
+        for (int i = _bombLeft; i < _maxBomb; i++)
+        {
+            _bombImages[i].SetActive(false);
         }
     }
 
